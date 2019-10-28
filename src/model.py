c0bd996fb2ff54404f82e1926518455e809af892
Nikita Nikitin <Nikita-fin@yandex.ru>
2019-10-28 12:57:28 +0300

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,6 +1,15 @@
 import numpy as np
-import OpenTextbot.src.Algebra as Algebra
+#import OpenTextbot.src.Algebra as Algebra
+from sklearn.cluster import KMeans
+#from sklearn.cluster import MiniBatchKMeans
 
-def GetArrayWE(model, index2D):
-  ...
-  return ArrayWE
+def GetCentroidArrayWE(ArrayWE, index2D):
+  CentroidArrayWE = []
+  for i in range(len(index2D)):
+    CurrentArrayWE = []
+    for j in range(len(index2D[i])):
+      CurrentArrayWE.append(ArrayWE[index2D[i][j]])
+    kmeans = KMeans(n_clusters=1, random_state=0).fit(CurrentArrayWE)
+    CentroidWE = kmeans.cluster_centers_
+    CentroidArrayWE.append(CentroidWE[0])
+  return CentroidArrayWE
