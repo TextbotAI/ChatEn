94509d034a777e5181dee078fcd7e976af625b45
Nikita Nikitin <Nikita-fin@yandex.ru>
2019-10-28 15:02:18 +0300

    Update model.py

--- src/model.py
+++ src/model.py
@@ -4,15 +4,18 @@ from sklearn.cluster import KMeans
 #from sklearn.cluster import MiniBatchKMeans
 
 def GetCentroidArrayWE(ArrayWE, index2D):
-  '''If some word consists of several tokens we get its' centroid.
+  '''If some word consists of several tokens we get their centroid.
   
   '''
   CentroidArrayWE = []
   for i in range(len(index2D)):
     CurrentArrayWE = []
-    for j in range(len(index2D[i])):
-      CurrentArrayWE.append(ArrayWE[index2D[i][j]])
-    kmeans = KMeans(n_clusters=1, random_state=0).fit(CurrentArrayWE)
-    CentroidWE = kmeans.cluster_centers_
-    CentroidArrayWE.append(CentroidWE[0])
+    if (len(index2D[i]) == 1):
+      CentroidArrayWE.append(ArrayWE[index2D[i][0]])
+    else:
+      for j in range(len(index2D[i])):
+        CurrentArrayWE.append(ArrayWE[index2D[i][j]])
+      kmeans = KMeans(n_clusters=1, random_state=0).fit(CurrentArrayWE)
+      CentroidWE = kmeans.cluster_centers_
+      CentroidArrayWE.append(CentroidWE[0])
   return CentroidArrayWE
